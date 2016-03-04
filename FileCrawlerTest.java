c56ba174b416b42ea2795f5dea9c798af3052753
umadulhassan <13bscsuhassan@seecs.edu.pk>
2016-03-04 12:53:16 +0500

    Create FileCrawlerTest.java

---
+++ FileCrawlerTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filecrawler;
+
+import java.io.File;
+import java.util.Collection;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Umad Rai
+ */
+public class FileCrawlerTest {
+    
+    public FileCrawlerTest() {
+    }
+
+    /**
+     * Test of addTree method, of class FileCrawler.
+     */
+    @Test
+    public void testAddTree() {
+        System.out.println("addTree");
+        File file = null;
+        Collection<File> all = null;
+        FileCrawler.addTree(file, all);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of main method, of class FileCrawler.
+     */
+   
+    
+}
